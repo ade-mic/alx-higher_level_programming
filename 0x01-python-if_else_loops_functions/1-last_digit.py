9bb0a08bae6d880fadca91742f98f2a9b815ab36
Ademola Aina <ademic.aina@gmail.com>
2023-10-03 05:26:10 -0700

    1 corr

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,4 @@ elif last_digit == 0:
     string = 'and is 0'
 else:
     string = 'and is less than 6 and not 0'
-print(f'Last digit of {number} is {last_digit} and is {string}')
+print(f'Last digit of {number} is {last_digit} {string}')
