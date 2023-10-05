66bc903c9b23805882617b295cfc1314fa44deeb
Ademola Aina <ademic-aina@gmail.com>
2023-10-05 18:18:09 +0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     n_argv = len(args)
 
     if n_argv == 1:
-        print('{} arguements.'.format(0))
+        print('{} arguments.'.format(0))
     elif n_argv == 2:
-        print('{} arguement:'.format(1))
+        print('{} argument:'.format(1))
         print('{}: {}'.format(1, args[1]))
     else:
-        print('{} arguements:'.format(n_argv - 1))
+        print('{} arguments:'.format(n_argv - 1))
         for i in range(1, n_argv):
             print('{}: {}'.format(i, args[i]))
