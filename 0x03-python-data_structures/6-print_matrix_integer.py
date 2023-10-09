bf87a6cdae21ce7757aef3604a2360b1edc15784
Ademola Aina <ademic-aina@gmail.com>
2023-10-09 14:06:44 +0800

    matrix'

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for vector in matrix:
-        for i in range(len(vector)):
-            if i < len(vector) - 1:
-                print('{:d}'.format(vector[i]), end=' ')
-            else:
-                print('{:d}'.format(vector[i]), end='\n')
+    if matrix == [[]]:
+        print(''.format('\n'), end='\n')
+    else:
+        for vector in matrix:
+            for i in range(len(vector)):
+                if i < len(vector) - 1:
+                    print('{:d}'.format(vector[i]), end=' ')
+                else:
+                    print('{:d}'.format(vector[i]), end='\n')
