497fe7f794d1f711b122a040a8dea7bf69780340
Ademola Aina <ademic-aina@gmail.com>
2023-10-10 00:16:52 +0800

    add tuple

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     last_idx = len(my_list) - 1
     while last_idx >= 0:
         print('{:d}'.format(my_list[last_idx]))
