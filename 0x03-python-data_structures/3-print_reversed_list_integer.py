b7ef2d9c31efb0c246aa901693d87d723f28fe55
Ademola Aina <ademic-aina@gmail.com>
2023-10-09 09:37:01 +0800

    print list reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    last_idx = len(my_list) - 1
+    while last_idx >= 0:
+        print('{}'.format(my_list[last_idx]))
+        last_idx -= 1
