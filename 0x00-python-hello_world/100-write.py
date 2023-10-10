9f00035ada83e92da47e70a2d3e1a5d89ab29d82
Ademola Aina <ademic.aina@gmail.com>
2023-10-09 19:58:45 -0700

    100 0x00

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    sys.stdout.write('and that piece of art is useful \
-    - Dora Korpar, 2015-10-19\n')
-    sys.exit(1)
+import sys
+sys.stdout.write('and that piece of art is useful \
+- Dora Korpar, 2015-10-19\n')
+sys.exit(1)
