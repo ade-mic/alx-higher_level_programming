7b737f3af7cbb6e27c590728cb5922e7d7f6f54f
Ademola Aina <ademic-aina@gmail.com>
2023-10-11 22:52:40 +0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """
-    Write a function that returns a key 
+    Write a function that returns a key
     with the biggest integer value.
     """
     if a_dictionary is not None:
