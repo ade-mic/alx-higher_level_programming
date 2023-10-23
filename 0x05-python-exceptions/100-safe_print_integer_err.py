ef34d26ba6ee1a128505f2dd3d664517d31b8003
Ademola Aina <ademic-aina@gmail.com>
2023-10-23 22:26:35 +0800

    100 check std err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,8 @@ def safe_print_integer_err(value):
         print('{:d}'.format(value), end='\n')
         return True
     except ValueError as err:
-        print('Exception: ', err, file=sys.stderr)
+        sys.stderr.write('Exception: {}\n'.format(err))
+        return False
     except TypeError as err:
-        print('Exception: ', err, file=sys.stderr)
+        sys.stderr.write('Exception: {}\n'.format(err))
         return False
