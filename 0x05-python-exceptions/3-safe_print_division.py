a5e7b1878235bf90af4cc9c8283d9aa2fbf65ff8
Ademola Aina <ademic-aina@gmail.com>
2023-10-23 17:52:17 +0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("{:d}".format(a / b))
-        return ()
-    except:
-        
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print('Inside result: {}'.format(result))
+        return result
