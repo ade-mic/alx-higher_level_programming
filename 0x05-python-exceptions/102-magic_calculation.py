5596a1924d06871931da8915588dd4a46b95a330
Ademola Aina <ademic-aina@gmail.com>
2023-10-24 10:26:04 +0800

    102 magic byte code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import dis
+
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -7,6 +10,8 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
         except Exception:
             pass
-        res = res + (a ** b) / i
-    res = res + a + b
-    return res
+        result = result + (a ** b) / i
+    result = result + a + b
+    return result
+
+dis.dis(magic_calculation)
