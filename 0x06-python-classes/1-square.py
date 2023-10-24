e9f2c8c4c0b162a4f5483315e55cbd9e3fc02575
Ademola Aina <ademic-aina@gmail.com>
2023-10-25 00:11:25 +0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class.
+
+The Square class is used to represent and manipulate squares.
+"""
+
+
+class Square:
+    """
+    An empty class Square that defines a square
+    the class is empty.
+
+    Attributes:
+        It has no method or attributes yet
+    """
+    def __init__(self, size):
+        """The constructor for Square class.
+
+        Args:
+            side (float): The length of the side of the square in units.
+        """
+        self.__size = size
+
+
