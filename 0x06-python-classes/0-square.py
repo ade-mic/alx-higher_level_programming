29bebf7461eeec3dd6a886154c206bab8666858e
Ademola Aina <ademic-aina@gmail.com>
2023-10-24 23:46:55 +0800

    doc string requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,6 +3,8 @@ class Square:
     """
     An empty class Square that defines a square
     the class is empty.
-    It has no method or attributes yet
+
+    Attributes:
+        It has no method or attributes yet
     """
     pass
