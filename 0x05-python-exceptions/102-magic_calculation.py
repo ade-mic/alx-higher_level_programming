eda1aeaf24aff4c74a091bcb7396e40401901d4e
Ademola Aina <ademic-aina@gmail.com>
2023-10-24 10:28:36 +0800

    check code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import dis
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -13,6 +10,3 @@ def magic_calculation(a, b):
         result = result + (a ** b) / i
     result = result + a + b
     return result
-
-
-dis.dis(magic_calculation)
