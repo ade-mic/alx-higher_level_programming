4e9f499d39cff061ef7a1172e9b93b4e246eaa3c
Ademola Aina <ademic-aina@gmail.com>
2023-10-25 07:26:25 +0800

    check again

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -83,8 +83,8 @@ class Square:
         if self.__size == 0:
             print('')
         else:
-            if self.__position[1] <= 0:
-                print('')
+            # if self.__position[1] <= 0:
+            #     print('')
             for i in range(self.__size):
                 print("{}{}".
                       format(' ' * self.__position[0], '#' * self.__size))
