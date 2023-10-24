5894123fd3f450e79b34696d9deebd0f809349fd
Ademola Aina <ademic-aina@gmail.com>
2023-10-24 23:56:31 +0800

    doc string requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class.
+
+The Square class is used to represent and manipulate squares.
+"""
 class Square:
     """
     An empty class Square that defines a square
