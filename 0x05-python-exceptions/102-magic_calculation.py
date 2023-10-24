8bc49b57e3324ccb3f74018f4a7f65898892761c
Ademola Aina <ademic-aina@gmail.com>
2023-10-24 10:15:16 +0800

    bytcode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a,b):
+    res = 0
+    for i in range (1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+        except Exception:
+            pass
+        res = res + (a ** b) / i
+    res = res + a + b
+    return res
