16a4980dcab0741c94f17ed148e7dbddefd85bf5
Ademola Aina <ademic-aina@gmail.com>
2023-10-25 00:46:06 +0800

    pycodestyle 4 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,7 +13,7 @@ class Square:
 
     Attributes:
         size(int):  an int type that must >= 0 to represent size of square
-    
+
     Methods:
         __init__(size): The constructor for Square class.
         area(): Returns the current square area
@@ -23,7 +23,7 @@ class Square:
 
         Args:
             size (int): The length of the side of the square in units.
-        
+
         Raises:
             TypeError: if not an integer
             ValueError: if <= 0
@@ -33,7 +33,7 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
-    
+
     def area(self):
         """Return the square area
         Args:
