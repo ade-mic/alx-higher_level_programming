e4b29ffab8a80eb2ad7a84806d147f7c7489a873
Ademola Aina <ademic-aina@gmail.com>
2023-11-01 10:57:39 +0800

    100-magic_string.py test

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #/usr/bin/python3
 def magic_string():
-    magic_string.counter += 1
-    return ("BestSchool, " * magic_string.counter + "BestSchool")
-magic_string.counter = -1
+    magic_string.counter = getattr(magic_string, 'counter', -1) + 1
+    return "BestSchool, " * magic_string.counter + "BestSchool"
