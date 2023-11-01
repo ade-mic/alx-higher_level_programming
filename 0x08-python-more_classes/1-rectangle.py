35082cf9692ca0d4512365a93a0cc4b489fa9495
Ademola Aina <ademic-aina@gmail.com>
2023-11-01 15:57:40 +0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             width(int)
             height(int)
         """
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
