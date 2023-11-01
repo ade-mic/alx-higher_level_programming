ab5d090d8baec5f978f1e22e6164fe3e095d5f2c
Ademola Aina <ademic-aina@gmail.com>
2023-11-01 13:53:29 +0800

    101 pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,4 +4,6 @@ class LockedClass:
         if name == 'first_name':
             self.__dict__[name] = value
         else:
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+            raise AttributeError(
+                f"'LockedClass' object has no attribute{name}'"
+            )
