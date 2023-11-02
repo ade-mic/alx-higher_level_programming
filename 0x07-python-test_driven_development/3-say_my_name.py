f8485318092d40fdb2af7aa42008ed4607161203
Ademola Aina <ademic-aina@gmail.com>
2023-11-02 13:53:22 +0800

    2-say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+"""
+a function that prints My name is <first name> <last name>
+Prototype: def say_my_name(first_name, last_name=""):
+first_name and last_name must be strings otherwise, raise a TypeError exception
+with the message first_name must be a string or last_name must be a string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """say_my_name
+        Args:
+            first_name(str)
+            last_name(str)
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print('My name is {} {}'.format(first_name, last_name))
