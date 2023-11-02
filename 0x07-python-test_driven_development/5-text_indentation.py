f49fea624111d09044399ccca15ced9e2bc29a5f
Ademola Aina <ademic-aina@gmail.com>
2023-11-02 18:59:07 +0800

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that prints a text with
+2 new lines after each of these characters: ., ? and :
+Prototype: def text_indentation(text):
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    line = text.split(".")
+    new_text = ".\n\n".join(line)
+    line = new_text.split("?")
+    new_text = "?\n\n".join(line)
+    line = new_text.split(":")
+    new_text = ":\n\n".join(line)
+    print(new_text)
