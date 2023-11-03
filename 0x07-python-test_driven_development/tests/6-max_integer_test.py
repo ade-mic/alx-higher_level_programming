d240b4d3d0c338fc4e56d44793aee118392aaca2
Ademola Aina <ademic-aina@gmail.com>
2023-11-03 10:37:50 +0800

    test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,11 +10,33 @@ class TestMaxInteger(unittest.TestCase):
     Test cases for max_integer()
     """
 
-    def test_max_int(self):
-        self.assertIsNone(max_integer())
+    def test_empty_list(self):
+        result = max_integer([])
+        self.assertIsNone(result)
+
+    def test_single_element_list(self):
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
+    def test_positive_numbers(self):
+        result = max_integer([1, 2, 3, 4, 5])
+        self.assertEqual(result, 5)
+   
+    def test_negative_numbers(self):
+        result = max_integer([-1, -2, -3, -4, -5])
+        self.assertEqual(result, -1)
+
+    def test_mixed_num(self):
+        res = max_integer([1, 2, -1, -10])
+        self.assertEqual(res, 2)
+
+    def test_empty_params(self):
+        res = max_integer()
+        self.assertIsNone(res)
+
+    def test_param_list(self):
         a = [1, 2, 3]
         result = max_integer(a)
         self.assertIsInstance(a, list)
-        self.assertEqual(result, 3)
         self.assertIsInstance(result, int)
     
