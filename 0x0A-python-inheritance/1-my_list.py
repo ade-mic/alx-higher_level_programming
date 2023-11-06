66e759a908c512420e2d74cfe2bf60bad48375ce
Ademola Aina <ademic-aina@gmail.com>
2023-11-06 20:26:26 +0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int
+"""
+
+
+class myList(list):
+    """
+    iherits from the list class or type
+    Args: 
+        list (list)
+    Methods:
+        prints_sorted: prints the list in ascending order
+    """
+def print_sorted(self, listItem):
+    """
+    prints the list in ascending order
+    Args:
+        self (list)
+        listItem (list): integers to be printed in sorted array
+    """
+    item = listItem[:].sorted()
