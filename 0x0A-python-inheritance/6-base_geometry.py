9903c245af42df877461803328087d060577228f
Ademola Aina <ademic-aina@gmail.com>
2023-11-07 09:25:00 +0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module contain a class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Method:
+        area(self)
+    Attribute: 
+        None
+    """
+    
+
+    def area(self):
+        """
+        Nothing implemeted yet
+        Return: 
+            string: area() is not implemented
+        """
+        return "area() is not implemented"
