9a50bde801d408a880654f38391beb53a1c7e90f
Ademola Aina <ademic-aina@gmail.com>
2023-11-07 09:16:27 +0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,15 @@ otherwise False
 
 
 def inherits_from(obj, a_class):
+    """
+    a function returns True id the obj is an instance of a_class
+    and False otherwise
+    Args:
+        obj(class)
+        a_class(class)
+    Returns:
+        Boolean
+    """
     obj_type = type(obj)
     for base_class in obj_type.__bases__:
         if issubclass(base_class, a_class):
