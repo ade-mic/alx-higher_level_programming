2fca41dc0a0435b55481f7a728b06e7a5b3f9c7e
Ademola Aina <ademic-aina@gmail.com>
2023-11-08 09:50:44 +0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """
-This module contain a function that read text file (UTF-8) and prints it to stdout
+This module contain a function that read text file (UTF-8)
+and prints it to stdout
 Prototype: def read_file(filename=""):
 """
 
+
 def read_file(filename=""):
+    """
+    Reads text file with UTF-8 encoding,
+    Args:
+        filename(filepath or directory)
+    """
     with open(filename, encoding='utf-8') as a_file:
         for line in a_file:
             print(line.rstrip(), end='\n')
