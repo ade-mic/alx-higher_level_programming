22b0c73aac180f0c38208a7da17383823a85fd65
Ademola Aina <ademic-aina@gmail.com>
2023-11-08 11:02:06 +0800

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This file contain  a function that load JSON file
+and create an object
+Prototype: def load_from_json_file(filename):
+"""
+
+
+def load_from_json_file(my_obj, filename):
+    """
+    This fuction load JSON file and create an object
+    Args:
+        filename(path)
+    """
+    # import json
+    import json
+    # open filename for writing
+    with open(filename) as a_file:
+        return json.load(a_file)
