109e0b64635a09b18daac0214ec422e7cf04906f
Ademola Aina <ademic-aina@gmail.com>
2023-11-10 11:51:14 +0800

    check again

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -25,8 +25,8 @@ def class_to_json(obj):
     # iterate iver the object's attributes
     for attr in dir(obj):
         # if attribute is private or special attributes
-        if attr.startswith('_'):
-            continue
+        # if attr.startswith('_'):
+        #     continue
         # get the value of the attribute
         value = getattr(obj, attr)
         # check if the value is callable (method or function)
