d83de4d53a579059234915dc9e55b5ac20942c10
Ademola Aina <ademic-aina@gmail.com>
2023-11-14 11:13:23 +0800

    1-base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This file contains a class name Base
+"""
+
+
+class Base:
+    """
+    private class attribute __nb_objects = 0
+    Method:
+        class constructor: def __init__(self, id=None)::
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Args:
+            self(instance of Base)
+            id (integer)
+        """
+        if id != None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
+
---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base import Base
+import unittest
+
+class TestBase(unittest.TestCase):
+    def TestPublicAttr(self):
+        model = Base()
+        self.assertEqual(model.id, 1) 
