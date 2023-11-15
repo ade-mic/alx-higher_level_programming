6414e283be03797d6945ebf3372d935674708a20
Ademola Aina <ademic-aina@gmail.com>
2023-11-15 14:53:57 +0800

    to__dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -76,3 +76,13 @@ class Square(Rectangle):
                         self.x = value
                     elif key == 'y':
                         self.y = value
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a
+        Rectangle instance
+        """
+        return {'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y}
