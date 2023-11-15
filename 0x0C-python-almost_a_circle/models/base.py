d04d3de654aeb2bcefeb467df83d5c294157e383
Ademola Aina <ademic-aina@gmail.com>
2023-11-15 15:32:29 +0800

    to_json method in baseclass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This file contains a class name Base
 """
+import json
 
 
 class Base:
@@ -23,3 +24,16 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Args:
+            list_dictionaries (list of dictionaries)
+        Returns
+            JSON string representation of list_dictionarie
+            If list_dictionaries is None or empty, return the string: "[]"
+        """
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
