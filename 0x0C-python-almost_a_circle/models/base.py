245d72ffbdeb1cb1baf6252da9f11f0d5be50db1
Ademola Aina <ademic-aina@gmail.com>
2023-11-15 15:54:53 +0800

    to_json_string list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,6 +24,7 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Args:
