81f17142faf9eb6a0e6110d50e5e5e1a882f4b3c
Ademola Aina <ademic-aina@gmail.com>
2023-11-15 13:50:55 +0800

    square fix __str__ overload

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,5 +12,11 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Class Constructor
+        """
         super().__init__(size, size, x, y, id)
-        
+    
+    def __str__(self):
+        return '[{}] ({}) {}/{} - {}'.format(type(self).__name__,
+                                            self.id, self.x, self.y, self.width)
