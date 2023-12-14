ebee688cdfbba18582f08b035371057206bfa378
Ademola Aina <ademic.aina@gmail.com>
2023-12-13 20:08:07 -0800

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- prints the full description of the table first_table from database
 -- hbrn_0c_0 in MYSQL server
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+DESC first_table;
