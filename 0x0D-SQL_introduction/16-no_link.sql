483bf69c04e51c9a7bbb519730a2e7e0adc07da2
Ademola Aina <ademic.aina@gmail.com>
2023-12-13 23:32:45 -0800

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the datatbase hbtn_0c_0
-SELECT score, name FROM second_table WHERE score IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
