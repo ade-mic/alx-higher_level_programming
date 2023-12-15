b63995f18ecf0e9da0b4de7e1c9d51694f9d0c91
Ademola Aina <ademic.aina@gmail.com>
2023-12-14 17:59:10 -0800

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS
 hbtn_0d_usa.cities (
 id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-state_id NOT NULL FOREIGN KEY REFERENCES states (id),
+state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
 name VARCHAR(256) NOT NULL
 );
