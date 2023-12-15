3dae65201f88b88173e9792bda588f048b688394
Ademola Aina <ademic.aina@gmail.com>
2023-12-15 02:05:11 -0800

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all the cities of califonia that can be found in the databae hbtn_0d_usa
 SELECT id, name FROM cities
-NATURAL JOIN states ON cities.state_id = states.id
-WHERE states.name='California' ORDER BY cities.id DESC;
+WHERE cities.state_id = states.id
+AND states.name='California' ORDER BY cities.id ASC;
