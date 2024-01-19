b394368c1cde109fe9ca5720654c2d90af8fe81d
ade-mic <ademic.aina@gmail.com>
2024-01-19 03:28:55 +0100

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- covert the database
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- convert the table
+ALTER TABLE first_table CONVERT TO CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci
+
+-- convert the field
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- displays the number of records with id=89 in the first_table
--- of the the databasr hbtn_0c_0 in MYSQL server
-SELECT COUNT(*)FROM first_table WHERE id = 89
+-- of the the database hbtn_0c_0 in MYSQL server
+SELECT COUNT(*) FROM first_table WHERE id = 89
