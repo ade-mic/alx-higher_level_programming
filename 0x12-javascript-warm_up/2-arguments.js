c64dcb22b98db6613884e54329a69ea94166afd8
ade-mic <ademic.aina@gmail.com>
2024-02-12 12:56:41 +0100

    change dire

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length <= 2) {
+let argvLen = argv.length;
+if (argvLen <= 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argvLen === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
