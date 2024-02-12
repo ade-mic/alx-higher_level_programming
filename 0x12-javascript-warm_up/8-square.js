824e3828a5820986d8d50d4a62adc7d6921c83f0
ade-mic <ademic.aina@gmail.com>
2024-02-12 15:28:50 +0100

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (isNaN(size) || size === undefined) {
+  console.log('Missing size');
+} else {
+  const width = size;
+  let height = size;
+  while (height > 0) {
+    console.log('x'.repeat(width));
+    height--;
+  }
+}
