191cd2b7492a0e34eccf15f254e259bc7a928dd2
ade-mic <ademic.aina@gmail.com>
2024-02-12 14:49:13 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const number = Number(process.argv[2]);
 if (isNaN(number) || number === undefined) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + Number(number));
+  console.log('My number: ' + Number(number));
 }
