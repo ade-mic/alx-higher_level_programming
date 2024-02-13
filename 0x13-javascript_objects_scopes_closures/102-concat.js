d50f09be17d0735d18c16a1cbb07cce49e483916
ade-mic <ademic.aina@gmail.com>
2024-02-13 19:58:24 +0100

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const [fileA, fileB, fileC] = process.argv.slice(2);
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+    fs.writeFile(fileC, dataA + '\n' + dataB + '\n', (err) => {
+	    if (err) throw err;
+    });
+  });
+});
