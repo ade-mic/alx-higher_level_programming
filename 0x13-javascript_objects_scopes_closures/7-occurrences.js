4c5a9e5c7ceba5a5d3116d953507318628496cae
ade-mic <ademic.aina@gmail.com>
2024-02-13 16:55:21 +0100

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-	    occurences++;
+      occurences++;
     }
   }
   return occurences;
