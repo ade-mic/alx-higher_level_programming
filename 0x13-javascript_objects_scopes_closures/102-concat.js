e8b06161b210ae3b61df036d239367f8edbcfce4
ade-mic <ademic.aina@gmail.com>
2024-02-13 20:36:21 +0100

    you

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,7 +6,7 @@ fs.readFile(fileA, 'utf8', (err, dataA) => {
   if (err) throw err;
   fs.readFile(fileB, 'utf8', (err, dataB) => {
     if (err) throw err;
-    fs.writeFile(fileC, dataA + '\n' + dataB + '\n', (err) => {
+    fs.writeFile(fileC, dataA + dataB + '\n', (err) => {
       if (err) throw err;
     });
   });
