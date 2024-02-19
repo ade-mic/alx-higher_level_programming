3f4ea9a7902fd70bf46fe7ab0c2d4e89aae80b7b
ade-mic <ademic.aina@gmail.com>
2024-02-19 13:05:18 +0100

    model_state.py_check

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 """
-a python file that contains the class definition of a State and an
+a python file that contains the class definition
+of a State and an
 instance Base = declarative_base():
-
-State class:
-inherits from Base Tips
-links to the MySQL table states
-class attribute id that represents a column of an auto-generated,
-unique integer, can’t be null and is a primary key
-class attribute name that represents a column of a string with maximum 128
-characters and can’t be null
 """
 # Import SQLAlchemy and other required modules
 from sqlalchemy import create_engine, Column, Integer, String
