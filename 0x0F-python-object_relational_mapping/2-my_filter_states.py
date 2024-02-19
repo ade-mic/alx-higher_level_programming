740370b3d1586c99209e6bde1e51ca94e2f494ac
ade-mic <ademic.aina@gmail.com>
2024-02-19 11:50:23 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script list all state wth a name starting where name matches arguement provided
+This script list all state wth a name starting
+where name matches arguement provided
 """
 import MySQLdb
 from sys import argv
@@ -13,8 +14,8 @@ def main():
     db = MySQLdb.connect(user=argv[1], password=argv[2],
                          host='localhost', port=3306, database=argv[3])
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name='" + argv[4] + "' ORDER BY states.id ASC"
-    #val = (argv[4],)
+    sql = "SELECT * FROM states WHERE name='" + argv[4] + "'\
+        ORDER BY states.id ASC"
     cursor.execute(sql)
     rows = cursor.fetchall()
     for row in rows:
