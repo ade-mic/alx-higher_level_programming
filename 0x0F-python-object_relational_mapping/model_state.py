3c04790557070131dd34e1037b89ee7790ca380b
ade-mic <ademic.aina@gmail.com>
2024-02-19 13:24:14 +0100

    6-model_state.sql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,43 +10,36 @@ from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 
 
-def main():
-    """
-    Program main entrance
-    """
-    # Define a class that representsthe State table
+ # Define a class that representsthe State table
 
-    
-    class State(Base):
-        """
-        State class:
-        inherits from Base Tips
-        links to the MySQL table states
-        class attribute:
-              (id): that represents a column of an auto-generated,
-                unique integer, can’t be null and is a primary key
-              (name) that represents a column of a
-                    string with maximum 128 characters and can’t be null
-        """
+# create a base class that contains the metadata and a constructor
+Base = declarative_base()
+class State(Base):
+    """
+    State class:
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute:
+        (id): that represents a column of an auto-generated,
+          unique integer, can’t be null and is a primary key
+        (name) that represents a column of a
+            string with maximum 128 characters and can’t be null
+    """
         # specify the table name and engine
-        __tablename__ = 'states'
-        __table_args__ = {'mysql_engine': 'InnoDB'}
+    __tablename__ = 'states'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
 
-        # Define the columns and their types
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128))
+    # Define the columns and their types
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
 
         # Define a constructor that accepts the column values as arguements
-        def __init__(self, name):
-            self.name = name
+    def __init__(self, name):
+        self.name = name
 
 
-if __name__ == "__main__":
     # Create an engine that connects to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3], pool_preping=True))
-    # create a base class that contains the metadata and a constructor
-    Base = declarative_base()
-    main()
-    # Create the table in the database
-    Base.metadata.create_all(engine)
+# Create the table in the database
+Base.metadata.create_all(engine)
