31f3585c54bc702c9853eae6dcb534160610c44a
ade-mic <ademic.aina@gmail.com>
2024-02-19 16:54:10 +0100

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-a script that deletes all State objects 
+a script that deletes all State objects
 with a name containing the letter a from the database
 """
 from sqlalchemy import create_engine, Column, Integer, String
