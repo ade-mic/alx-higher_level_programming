4bbcfdfe98b2cc5b803b9ab71c3a727f14580701
ade-mic <ademic.aina@gmail.com>
2024-02-19 12:29:41 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,12 +6,24 @@ the script takes 3 argument : mysql username, mysql password and database name
 # import libraries
 import MySQLdb
 from sys import argv
-# create connection
-db = MySQLdb.connect(user=argv[1], password=argv[2],
-                     host='localhost', port=3306, database=argv[3])
-cursor = db.cursor()
-sql = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
-cursor.execute(sql)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+
+
+def main():
+    """
+    Main Entraince of the program
+    """
+    # create connection
+    db = MySQLdb.connect(user=argv[1], password=argv[2],
+                         host='localhost', port=3306, database=argv[3])
+    cursor = db.cursor()
+    sql = "SELECT cities.id, cities.name, states.name\
+          FROM cities INNER JOIN states ON cities.state_id = states.id\
+          ORDER BY cities.id ASC"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
