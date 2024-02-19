a60ecdb5b8d0a40420205b679538b9b59f69ca09
ade-mic <ademic.aina@gmail.com>
2024-02-19 12:11:03 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,13 +5,23 @@ This script list all state wth a name starting where name matches arguement prov
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(user=argv[1], password=argv[2],
-                     host='localhost', port=3306,
-                     database=argv[3])
-cursor = db.cursor()
-sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
-val = (argv[4],)
-cursor.execute(sql, val)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+
+def main():
+    """
+    The main entrance to the program
+    """ 
+    db = MySQLdb.connect(user=argv[1], password=argv[2],
+                        host='localhost', port=3306,
+                        database=argv[3])
+    cursor = db.cursor()
+    sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
+    val = (argv[4],)
+    cursor.execute(sql, val)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
+
