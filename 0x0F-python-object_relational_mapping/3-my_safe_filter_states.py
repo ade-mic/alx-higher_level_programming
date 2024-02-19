e900bf31d0021ef1e5e21c03e40f73faa9f5bdc7
ade-mic <ademic.aina@gmail.com>
2024-02-19 12:26:03 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script list all state wth a name starting where name matches arguement provided
+This script list all state wth a name
+starting where name matches arguement provided
 """
 import MySQLdb
 from sys import argv
@@ -9,12 +10,13 @@ from sys import argv
 def main():
     """
     The main entrance to the program
-    """ 
+    """
     db = MySQLdb.connect(user=argv[1], password=argv[2],
-                        host='localhost', port=3306,
-                        database=argv[3])
+                         host='localhost', port=3306,
+                         database=argv[3])
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id ASC"
+    sql = "SELECT * FROM states WHERE REGEXP_LIKE(name, %s, 'c')\
+          ORDER BY states.id ASC"
     val = (argv[4],)
     cursor.execute(sql, val)
     rows = cursor.fetchall()
@@ -24,4 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
