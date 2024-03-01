ea357d3b3132c72f6d938c43d63c3cb709aecc93
ade-mic <ademic.aina@gmail.com>
2024-03-01 01:07:10 +0100

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"
+#!/bin/bash
+curl -sI "$1" | grep 200 | curl -s "$1"
