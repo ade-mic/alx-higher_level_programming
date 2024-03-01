28327fcac323538f8b6cfb56eef9f158587c1e2c
ade-mic <ademic.aina@gmail.com>
2024-03-01 18:37:08 +0100

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable found
+in the header of the response.
+"""
+
+
+def main():
+    """ Main fuction to not allow the code be executed
+    when imported """
+    import urllib.request
+    import os
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        x_request_id = response.getheader("X-Request-Id")
+        print(x_request_id)
+
+
+
+if __name__ == "__main__":
+    main()
