030c454599edb2a451f4735fd20b1020ecb9f859
ade-mic <ademic.aina@gmail.com>
2024-03-01 01:12:28 +0100

    2-delete.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI "$1" | grep 200 | curl -s "$1"
+curl -sI "$1" | grep 200 | curl "$1"
