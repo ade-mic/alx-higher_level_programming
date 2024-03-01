7beeb6ebfe0649feafc5d0350ad143d53b6b82d9
ade-mic <ademic.aina@gmail.com>
2024-03-01 18:14:18 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,20 @@
 """
 A Python script that fetches https://alx-intranet.hbtn.io/status
 """
-""" Main fuction to not allow the code be executed
-when imported """
-import urllib.request
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    hbtn_page = response.read()
-    print('- type: {}'.format(type(hbtn_page)))
-    print('- content: {}'.format(hbtn_page))
-    print('- utf8 content: {}'.format(hbtn_page.decode('utf-8')))
+
+
+def main():
+    """ Main fuction to not allow the code be executed
+    when imported """
+    import urllib.request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        hbtn_page = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(hbtn_page)))
+        print('\t- content: {}'.format(hbtn_page))
+        print('\t- utf8 content: {}'.format(hbtn_page.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
