2d24cdb1a293e27a07c77ec28306191bdf90eb3d
ade-mic <ademic.aina@gmail.com>
2024-03-01 17:58:05 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+def main():
+    """ Main fuction to not allow the code be executed
+    when imported """
+    import urllib.request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        hbtn_page = response.read()
+        print('Body response:')
+        print('- type: {}'.format(type(hbtn_page)))
+        print('- content: {}'.format(hbtn_page))
+        print('- utf8 content: {}'.format(hbtn_page.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
