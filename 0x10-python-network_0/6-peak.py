3b4ea8f847a3de0e167841a97978663397e00974
ade-mic <ademic.aina@gmail.com>
2024-03-01 02:06:14 +0100

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 """
  a function that finds a peak in a list of unsorted integers.
 """
+
+
 def find_peak(arr):
     """
      a function that finds a peak in a list of unsorted integers.
@@ -13,7 +15,8 @@ def find_peak(arr):
         mid = low + (high - low) // 2
 
         # Check if mid is a peak
-        if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == n - 1 or arr[mid + 1] <= arr[mid]):
+        if (mid == 0 or arr[mid - 1] <= arr[mid]) and\
+           (mid == n - 1 or arr[mid + 1] <= arr[mid]):
             return arr[mid]
 
         # If right neighbor is greater, search right half
