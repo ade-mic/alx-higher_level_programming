4426e39e2ad64df34cc47c2da3921db9228de3de
ade-mic <ademic.aina@gmail.com>
2024-03-01 01:30:09 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -siX allow '$1'| grep -i '^allow:'
+curl -sI -X OPTIONS "$1" | grep -i "allow" | awk '{print $2}'
