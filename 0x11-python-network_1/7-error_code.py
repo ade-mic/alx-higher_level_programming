0ac537e2293a20a4e889d0dacd73073d349247cd
ade-mic <ademic.aina@gmail.com>
2024-03-02 11:53:28 +0100

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A  Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response
+(decoded in utf-8)
+"""
+import requests
+from sys import argv
+
+
+def main():
+    """
+    main fuction to not allow the code be executed
+    when imported
+    """
+    req = requests.get(argv[1])
+    if req.status_code != requests.codes.ok:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
+
+if __name__ == "__main__":
+    main()
