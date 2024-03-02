cdb929eb94f119c92a05fec42a2c980c39e8806b
ade-mic <ademic.aina@gmail.com>
2024-03-02 12:15:12 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -15,10 +15,11 @@ def main():
     when imported
     """
     req = requests.get(argv[1])
-    if req.status_code == requests.codes.ok:
-        print(req.text)
-    elif req.status_code >= 400:
+    if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
+    else:
+          print(req.text)
+
 
 if __name__ == "__main__":
     main()
