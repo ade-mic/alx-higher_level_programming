4ddf0e6b986ec20e705058328601f0f7a0be47b4
ade-mic <ademic.aina@gmail.com>
2024-03-02 13:58:46 +0100

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+def main():
+    """
+    main fuction to not allow the code be executed
+    when imported
+    """
+    if len(argv) > 1:
+        params = {'q': argv[1]}
+    else:
+        params = {'q': ""}
+    # Send a POST request
+    base_url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(base_url, params=params)
+
+    # Check if the response is valid JSON
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
