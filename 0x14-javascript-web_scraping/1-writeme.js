3d976f0c59a8c92da6a6319c0720b1a158e39c1b
ade-mic <ademic.aina@gmail.com>
2024-03-26 14:49:08 +0100

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// read a file
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+});
