84eb7272d517875639cd07222c20223aacc2c825
ade-mic <ademic.aina@gmail.com>
2024-03-26 14:43:59 +0100

    semistandard0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-//read a file
-fs.readFile(process.argv[2], 'utf8', function(err, data){
-    if (err) {
-        console.log(err)
-    }
-    else {console.log(data)
-    }
+// read a file
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
