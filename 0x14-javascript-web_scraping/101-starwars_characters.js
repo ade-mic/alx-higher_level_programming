cb952c3f7c6e9ff4618a207a8d2f712f8db7184f
ade-mic <ademic.aina@gmail.com>
2024-03-27 10:18:31 +0100

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = 'https://swapi.dev/api/films/';
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Make a request to the Star Wars API to get the film data
+request(`${apiUrl}${movieId}`, (error, response, body) => {
+  if (error) {
+    console.error('An error occurred:', error);
+    return;
+  }
+
+  const film = JSON.parse(body);
+
+  // Create a function that will fetch a character name
+  const fetchCharacterName = (url, callback) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error('An error occurred:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      callback(character.name);
+    });
+  };
+
+  // Fetch each character's name in the order they appear in the film
+  film.characters.forEach((characterUrl) => {
+    fetchCharacterName(characterUrl, (name) => {
+      console.log(name);
+    });
+  });
+});
