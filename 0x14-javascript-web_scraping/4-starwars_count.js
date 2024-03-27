1eaacb4f6cacdffcb1f45b509659716d887b9124
ade-mic <ademic.aina@gmail.com>
2024-03-27 07:45:45 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const api = process.argv[2];
-//  prints the title of a Star Wars movie where the episode number matches a given integer.
-request(api, function (error, response, body) {
+const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(characterUrl, function (error, response, body) {
   if (error) throw error;
   else if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
-    const movieWithWedge = data.results.filter(movie =>
-      movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'
-      )).length;
-    console.log(movieWithWedge);
+    const films = data.films;
+    console.log(`${films.length}`);
   }
 });
