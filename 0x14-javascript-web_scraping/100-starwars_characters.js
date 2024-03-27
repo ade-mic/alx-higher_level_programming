3df5a8383f561c5209375778b90a1bf24915733c
ade-mic <ademic.aina@gmail.com>
2024-03-27 10:08:51 +0100

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
-let movieId = process.argv[2];  // Replace with your Movie ID
-let api = `https://swapi.dev/api/films/${movieId}/`;
+const movieId = process.argv[2]; // Replace with your Movie ID
+const api = `https://swapi.dev/api/films/${movieId}/`;
 
 request(api, function (error, response, body) {
   if (error) throw error;
   else if (!error && response.statusCode === 200) {
     const film = JSON.parse(body);
-    let characters = film.characters;
+    const characters = film.characters;
 
-    characters.forEach(function(character) {
+    characters.forEach(function (character) {
       request(character, function (error, response, body) {
         if (error) throw error;
         else if (!error && response.statusCode === 200) {
