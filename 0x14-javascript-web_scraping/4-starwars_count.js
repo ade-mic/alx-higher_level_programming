bad164379167e2290ee334eb8f9f4932b44312d5
ade-mic <ademic.aina@gmail.com>
2024-03-27 08:22:03 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
+const api = process.argv[2];
 const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-request(characterUrl, function (error, response, body) {
+request(api, function (error, response, body) {
   if (error) throw error;
   else if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
-    const films = data.films;
-    console.log(films.length);
+
+    const res = data.results.filter(movie =>
+      movie.characters.includes(characterUrl))
+    count = res.length 
+    console.log(count);
   }
 });
